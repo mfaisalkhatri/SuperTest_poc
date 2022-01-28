252d17d26e4168e12a2b9d110abb1ba4df2400f1
mfaisalkhatri <mohammadfaisalkhatri@gmail.com>
2022-01-29 00:45:22 +0530

    Updated get test as it was printing null in console as no error was thrown

--- test/getTest.spec.js
+++ test/getTest.spec.js
@@ -25,7 +25,7 @@ describe('Get API tests using supertest', () => {
 			.query({ page: '2' })
 			.set('Accept', 'application/json')
 			.set('Content-Type', 'application/json')
-			.end(function(err, res) {
+			.end(function (err, res) {
 				expect(res.statusCode).to.be.equal(200);
 				expect(res.body.page).to.be.equal(2);
 				expect(res.body.data[0].id).to.be.equal(7);
@@ -34,21 +34,24 @@ describe('Get API tests using supertest', () => {
 			});
 	});
 	it('should successfully pass the test for get api without query param', (done) => {
-		request(baseurl).get('/api/users/2').end(function(err, res) {
-			expect(res.statusCode).to.be.equal(200);
-			expect(res.body.data.id).to.be.equal(2);
-			expect(res.body.data.first_name).to.be.equal('Janet');
-			done();
-		});
+		request(baseurl)
+			.get('/api/users/2')
+			.end(function (err, res) {
+				expect(res.statusCode).to.be.equal(200);
+				expect(res.body.data.id).to.be.equal(2);
+				expect(res.body.data.first_name).to.be.equal('Janet');
+				done();
+			});
 	});
 
 	it('should successfully pass the test for get api with path param', (done) => {
 		let param = 1;
-		request('https://fakerestapi.azurewebsites.net').get('/api/v1/Authors/' + param).end(function(err, res) {
-			expect(res.statusCode).to.be.equal(200);
-			expect(res.body.id).to.be.equal(1);
-			console.log(err);
-			done();
-		});
+		request('https://fakerestapi.azurewebsites.net')
+			.get('/api/v1/Authors/' + param)
+			.end(function (err, res) {
+				expect(res.statusCode).to.be.equal(200);
+				expect(res.body.id).to.be.equal(1);
+				done();
+			});
 	});
 });
